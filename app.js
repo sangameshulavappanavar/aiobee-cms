f9a414cc6577119ff101da1b9b62cef234405fb4
Sangamesh U <sangamesh@Sangameshs-MacBook-Air.local>
2018-08-13 14:17:44 +0530

    database info

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/aiobee');
+mongoose.connect('mongodb://sangamesh:vcvula20966@ds213832.mlab.com:13832/aiobee');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
